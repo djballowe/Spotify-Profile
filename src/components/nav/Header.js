08a02c7463832054f50393d568f56a36b09269bf
djballowe <djballowe@gmail.com>
2022-10-08 13:35:36 -0700

    desktop header styles and navigation added

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import spotify from "../../images/spotify-logo.png";
 import Home from "@mdi/react";
 import { mdiHomeVariantOutline } from "@mdi/js";
@@ -12,6 +12,16 @@ import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   let navigate = useNavigate();
+  const [page, setPage] = useState(window.location.pathname);
+
+  const highlightPage = () => {
+    const location = window.location.pathname;
+    setPage(location);
+  };
+
+  useEffect(() => {
+    highlightPage();
+  });
 
   return (
     <div className="header-container">
@@ -20,32 +30,76 @@ const Header = () => {
         <li
           onClick={() => {
             navigate("/");
+            highlightPage();
+          }}
+          style={{
+            color: page === "/" ? "white" : "rgb(71, 71, 71)",
           }}
         >
+          <div
+            className="highlight-desktop"
+            style={{
+              visibility: page === "/" ? "visible" : "hidden",
+              opacity: page === "/" ? "1" : "0",
+            }}
+          ></div>
           <Home path={mdiHomeVariantOutline} size={1.4} /> Home
         </li>
         <li
           onClick={() => {
             navigate("artists");
+            highlightPage();
+          }}
+          style={{
+            color: page === "/artists" ? "white" : "rgb(71, 71, 71)",
           }}
         >
           {" "}
+          <div
+            className="highlight-desktop"
+            style={{
+              visibility: page === "/artists" ? "visible" : "hidden",
+              opacity: page === "/artists" ? "1" : "0",
+            }}
+          ></div>
           <Artist path={mdiAccountMusicOutline} size={1.4} /> Top Artists
         </li>
         <li
           onClick={() => {
             navigate("tracks");
+            highlightPage();
+          }}
+          style={{
+            color: page === "/tracks" ? "white" : "rgb(71, 71, 71)",
           }}
         >
           {" "}
+          <div
+            className="highlight-desktop"
+            style={{
+              visibility: page === "/tracks" ? "visible" : "hidden",
+              opacity: page === "/tracks" ? "1" : "0",
+            }}
+          ></div>
           <List path={mdiPlaylistMusicOutline} size={1.4} /> Top Tracks
         </li>
         <li
           onClick={() => {
             navigate("recent");
+            highlightPage();
+          }}
+          style={{
+            color: page === "/recent" ? "white" : "rgb(71, 71, 71)",
           }}
         >
           {" "}
+          <div
+            className="highlight-desktop"
+            style={{
+              visibility: page === "/recent" ? "visible" : "hidden",
+              opacity: page === "/recent" ? "1" : "0",
+            }}
+          ></div>
           <Clock path={mdiClockOutline} size={1.4} /> Top Tracks
         </li>
       </ul>
