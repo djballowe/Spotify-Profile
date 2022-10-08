bc54fe297d239333d6e24d461f40cedf43ecdac4
djballowe <djballowe@gmail.com>
2022-10-08 09:00:04 -0700

    router added to mobile menu

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Header from "./components/nav/Header";
-import Profile from "./components/Profile-Main/Profile";
 import MobileHeader from "./components/nav/MobileHeader";
-import Artists from "./components/Profile-Main/Artists";
 import Home from "./components/Home/Home";
 import TopArtists from "./components/Top Artists/TopArtists";
 import TopTracks from "./components/Top Tracks/TopTracks";
@@ -14,8 +12,8 @@ import Recent from "./components/Recent/Recent";
 function App() {
   return (
     <div className="App">
-      <MobileHeader />
       {/* <Header /> */}
+      <MobileHeader />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
--- src/components/nav/MobileHeader.js
+++ src/components/nav/MobileHeader.js
@@ -7,25 +7,52 @@ import List from "@mdi/react";
 import { mdiPlaylistMusicOutline } from "@mdi/js";
 import Clock from "@mdi/react";
 import { mdiClockOutline } from "@mdi/js";
+import { useNavigate } from "react-router-dom";
 
 const MobileHeader = () => {
+  let navigate = useNavigate();
+
   return (
     <div className="mobile-nav-container">
       <div className="highlight"></div>
-      <div className="nav-mobiles">
-        <Home path={mdiHomeVariantOutline} size={1.4} />
+      <div className="nav-mobiles" id="home">
+        <Home
+          path={mdiHomeVariantOutline}
+          size={1.4}
+          onClick={() => {
+            navigate("/");
+          }}
+        />
         <p>Home</p>
       </div>
-      <div className="nav-mobiles">
+      <div
+        className="nav-mobiles"
+        id="artists"
+        onClick={() => {
+          navigate("artists");
+        }}
+      >
         <Artist path={mdiAccountMusicOutline} size={1.4} />
         <p>Top Artist</p>
       </div>
-      <div className="nav-mobiles">
+      <div
+        className="nav-mobiles"
+        id="tracks"
+        onClick={() => {
+          navigate("tracks");
+        }}
+      >
         <List path={mdiPlaylistMusicOutline} size={1.4} />
         <p>Top Tracks</p>
       </div>
-      <div className="nav-mobiles">
-        <List path={mdiClockOutline} size={1.4} />
+      <div
+        className="nav-mobiles"
+        id="recent"
+        onClick={() => {
+          navigate("recent");
+        }}
+      >
+        <Clock path={mdiClockOutline} size={1.4} />
         <p>Recent</p>
       </div>
     </div>
